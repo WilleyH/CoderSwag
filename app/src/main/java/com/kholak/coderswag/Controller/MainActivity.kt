b8a5a1d549f44ca7b98302ab8c7cd5cc729f3e79
Casper Holak <33180890+WilleyH@users.noreply.github.com>
2017-11-28 23:25:02 +0100

    recyclerView

--- app/src/main/java/com/kholak/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/kholak/coderswag/Controller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.kholak.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import com.kholak.coderswag.Adapters.CategoryAdapter
 import com.kholak.coderswag.Model.Category
 import com.kholak.coderswag.R
 import com.kholak.coderswag.Services.DataService
@@ -10,15 +10,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-                android.R.layout.simple_list_item_1,
-                DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
     }
 }
