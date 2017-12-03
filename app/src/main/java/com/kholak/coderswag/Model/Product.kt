f3646f0ad44c48cbaf32edd43f466ed0891e907e
Casper Holak <33180890+WilleyH@users.noreply.github.com>
2017-12-03 09:24:05 +0100

    product view to be finished

--- app/src/main/java/com/kholak/coderswag/Model/Product.kt
+++ app/src/main/java/com/kholak/coderswag/Model/Product.kt
@@ -3,4 +3,4 @@ package com.kholak.coderswag.Model
 /**
  * Created by nsilska on 2017-11-28.
  */
-class Product (val title : String, val price : String, val Image : String)
+class Product (val title : String, val price : String, val image : String)
--- app/src/main/java/com/kholak/coderswag/Services/DataService.kt
+++ app/src/main/java/com/kholak/coderswag/Services/DataService.kt
@@ -18,10 +18,19 @@ object DataService {
             Product("Devslopes Graphic Beanie", "$18", "hat1"),
             Product("Devslopes Hat Black", "$20", "hat2"),
             Product("Devslopes Hat White", "$18", "hat3"),
+            Product("Devslopes Hat Snapback", "$22", "hat4"),
+            Product("Devslopes Graphic Beanie", "$18", "hat1"),
+            Product("Devslopes Hat Black", "$20", "hat2"),
+            Product("Devslopes Hat White", "$18", "hat3"),
             Product("Devslopes Hat Snapback", "$22", "hat4")
+
     )
 
     val hoodies = listOf(
+            Product("Devslopes Hoodie Gray", "$28", "hoodie1"),
+            Product("Devslopes Hoodie Red", "$32", "hoodie2"),
+            Product("Devslopes Gray Hoodie", "$28", "hoodie3"),
+            Product("Devslopes Black Hoodie", "$22", "hoodie4"),
             Product("Devslopes Hoodie Gray", "$28", "hoodie1"),
             Product("Devslopes Hoodie Red", "$32", "hoodie2"),
             Product("Devslopes Gray Hoodie", "$28", "hoodie3"),
